929c5084902e159da8069cdc8d2268666d6c21c1
Jakeagle <114176194+Jakeagle@users.noreply.github.com>
2024-12-14 20:40:58 -0600

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     try {
-      const response = await fetch("http://localhost:3000/login", {
+      const response = await fetch("https://tradewindsprototype-gnbgdcg4gsd4fwb8.centralus-01.azurewebsites.net/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
